1ffbc430528c4c1a1cba68544111756253debad3
qgtx64 <s.j.spence@durham.ac.uk>
2019-12-11 17:27:16 +0000

    debugging xml <-> dict

--- networking/translator.py
+++ networking/translator.py
@@ -6,7 +6,6 @@ Stefan Spence 04/10/19
 """
 import json
 import xmltodict
-import dicttoxml
 import sys
 import numpy as np
 from collections import OrderedDict
@@ -29,43 +28,45 @@ except ImportError:
 
 #### #### DExTer clusters #### ####
 
-def event_list():
+def event_list(name='', routinespec=False, indices=[], path=''):
     """Define a dictionary for DExTer's event
     list cluster since it's used several times."""
-    return OrderedDict([('Event name',''),
-            ('Routine specific event?',False),
-            ('Event indices',[]),
-            ('Event path','')])
+    return OrderedDict([('Event name',name),
+            ('Routine specific event?',routinespec),
+            ('Event indices',indices),
+            ('Event path',path)])
 
-def header_cluster():
+def header_cluster(skip=False, ename='', hide=False, ID='',
+        tsname='', populate=False, tslen=1, tunit=1, dora=0,
+        trigger=False, channel=0, av=0, gpibevent=0, gpibon=False):
     """Define a dictionary for DExTer's matrix header
     cluster since it's used several times."""
-    return OrderedDict([('Skip Step',False),
-            ('Event name',''),
-            ('Hide event steps',False),
-            ('Event ID',''),
-            ('Time step name',''),
-            ('Populate multirun',False),
-            ('Time step length',1),
-            ('Time unit',1),
-            ('Digital or analogue trigger?',0),
-            ('Trigger this time step?',False),
-            ('Channel',0),
-            ('Analogue voltage (V)',0),
-            ('GPIB event name',0),
-            ('GPIB on/off',False)])
+    return OrderedDict([('Skip Step',skip),
+            ('Event name',ename),
+            ('Hide event steps',hide),
+            ('Event ID',ID),
+            ('Time step name',tsname),
+            ('Populate multirun',populate),
+            ('Time step length',tslen),
+            ('Time unit',tunit),
+            ('Digital or analogue trigger?',dora),
+            ('Trigger this time step?',trigger),
+            ('Channel',channel),
+            ('Analogue voltage (V)',av),
+            ('GPIB event name',gpibevent),
+            ('GPIB on/off',gpibon)])
 
 def channel_names(length, values=None):
     """Define a dictionary for DExTer's channel names cluster
     since it's used several times. Can be initialised with 
     items but take care to give them the right length:
     values = [[hardware IDs],[names]]"""
-    if not values:
-        return OrderedDict([('Hardware ID',['']*length),
-                    ('Name',['']*length)])
+    if values and len(values[0])==length and len(values[1])==length:
+        return OrderedDict([('Hardware ID', values[0]),
+                    ('Name', values[1])])
     else:
-        return OrderedDict([('Hardware ID',['']*length),
-                    ('Name',['']*length)])
+        return OrderedDict([('Hardware ID', ['']*length),
+                    ('Name', ['']*length)])
 
 def analogue_cluster(length, values=None):
     """Define a dictionary for DExTer's analogue channels 
@@ -73,12 +74,12 @@ def analogue_cluster(length, values=None):
     initialised with items but take care to give them the 
     right length:
     values = [[voltages],[ramp?]]"""
-    if not values:
-        return OrderedDict([('Voltage',[0]*length),
-                    ('Ramp?',[False]*length)])
+    if values and len(values[0])==length and len(values[1])==length:
+        return OrderedDict([('Voltage', values[0]),
+                    ('Ramp?', values[1])])
     else:
-        return OrderedDict([('Voltage',[0]*length),
-                    ('Ramp?',[False]*length)])
+        return OrderedDict([('Voltage', [0]*length),
+                    ('Ramp?', [False]*length)])
 
 def nm_val(dic, key, conv=[]):
     """Return an ordered dictionary with the LV
@@ -103,9 +104,9 @@ def mk_dic(typ='Array', name='', size=1, dtyp='Cluster', data=[]):
         ('Dimsize' if typ=='Array' else 'NumElts', size),
         (dtyp, data)])
 
-def hf_seq(seq_dict, nsteps, nd, na, up=1):
-    """Expand the dictionaries required for the header and channels
-    of either the top or bottom half of the sequence.
+def wrap_hf(seq_dict, nsteps, nd, na, up=1):
+    """Shorthand to expand the dictionaries required for the header 
+    and channels of either the top or bottom half of the sequence.
     seq_dict: the python dictionary which the sequence is stored in
     nsteps: the number of time steps in the sequence
     nd: number of digital channels
@@ -150,7 +151,7 @@ def hf_seq(seq_dict, nsteps, nd, na, up=1):
                 [nm_val(fan, key) for key in ['Hardware ID', 'Name']])
             for fan in seq_dict['Experimental sequence cluster in'][speed + ' analogue names']]),
         mk_dic('Array', speed + ' analogue array', [na, nsteps], 'Cluster',
-            [OreredDict([
+            [OrderedDict([
                 ('Name', 'Analogue cluster'),
                 ('NumElts', 2),
                 ('Boolean', OrderedDict([('Name','Ramp?'), 
@@ -160,7 +161,8 @@ def hf_seq(seq_dict, nsteps, nd, na, up=1):
             ]) for j in range(na) for i in range(nsteps)]))
 
 def wrap_sequence(seq_dict):
-    """Add all the extra jargon that LabVIEW puts in XML files."""
+    """Add all the extra jargon that LabVIEW puts in XML files
+    to a dictionary containing the sequence."""
     numsteps = len(seq_dict['Experimental sequence cluster in']['Sequence header top'])
     numevents = len(seq_dict['Event list array in'])
     return OrderedDict([('LVData', OrderedDict([
@@ -187,19 +189,83 @@ def wrap_sequence(seq_dict):
                     ]) for event in seq_dict['Event list array in']])),
             ('Cluster', mk_dic(typ='Cluster', name='Experimental sequence cluster in', 
                 size=10, dtyp='Array',
-                data=[*hf_seq(seq_dict, numsteps, 56, 8, 1),
-                    *hf_seq(seq_dict, numsteps, 48, 32, 0)])),
+                data=[*wrap_hf(seq_dict, numsteps, 56, 8, 1),
+                    *wrap_hf(seq_dict, numsteps, 48, 32, 0)])),
             ('String', [nm_val(seq_dict, key)
                 for key in ['Routine name in', 'Routine description in']])
             ]))
         ]))
     ])
-    
+
+def find_item(list_dicts, item_name):
+    """Find the index in the list of dictionaries which has a
+    'Name' item with value item_name"""
+    for d in list_dicts:
+        if d['Name'] == item_name:
+            return d
+
+def strip_hf(seq_arrays, up=1):
+    """Shorthand to strip and reformat the dictionaries required for the header 
+    and channels of either the top or bottom half of the sequence.
+    seq_arrays: the list of arrays giving headers, channels, and names;
+        seq_dict['LVData']['Cluster']['Cluster']['Array']
+    up: 1 - the top half (fast channels), 0 - bottom half (slow)."""
+    title = 'Sequence header ' + 'top' if up else 'middle'
+    speed = 'Fast' if up else 'Slow'
+    nsteps= len(find_item(seq_arrays, title)['Cluster']) # number of timesteps
+    nd = len(find_item(seq_arrays, speed+' digital names')['Cluster']) # number of digital channels
+    na = len(find_item(seq_arrays, speed+' analogue names')['Cluster']) # number of analogue channels
+    return ((title,[
+        header_cluster(skip=find_item(head['Boolean'], 'Skip Step')['Val'], 
+            ename=find_item(head['String'], 'Event name')['Val'], 
+            hide=find_item(head['Boolean'], 'Hide event steps')['Val'], 
+            ID=head['I32']['Val'], 
+            tsname=find_item(head['String'], 'Time step name')['Val'], 
+            populate=find_item(head['Boolean'], 'Populate multirun')['Val'], 
+            tslen=head['DBL']['Val'], 
+            tunit=head['EW']['Val'], 
+            dora=head['Cluster'][0]['EW']['Val'],
+            trigger=head['Cluster'][0]['Boolean']['Val'], 
+            channel=head['Cluster'][0]['U8']['Val'], 
+            av=head['Cluster'][0]['DBL']['Val'], 
+            gpibevent=head['Cluster'][1]['EW']['Val'], 
+            gpibon=head['Cluster'][1]['Boolean']['Val']
+        ) for head in find_item(seq_arrays, title)['Cluster']]),
+        (speed + ' digital names', channel_names(nd, 
+            list(map(list, zip(*[ # transpose list
+                [find_item(cl['String'], 'Hardware ID')['Val'], 
+                find_item(cl['String'], 'Name')['Val']] 
+                for cl in find_item(seq_arrays, speed+' digital names')['Cluster']]))))),
+        (speed + ' digital channels', list(map(list, # reshape flattened list of digital channels
+            np.reshape([x['Val'] for x in find_item(seq_arrays, speed + ' digital channels')['Boolean']], (nsteps, nd))))),
+        (speed + ' analogue names', channel_names(na, 
+            list(map(list, zip(*[ # transpose list
+                [find_item(cl['String'], 'Hardware ID')['Val'], 
+                find_item(cl['String'], 'Name')['Val']] 
+                for cl in find_item(seq_arrays, speed+' analogue names')['Cluster']]))))),
+        (speed + ' analogue array', [analogue_cluster(na, list(map(list, zip(*[ # transpose list
+                [cl['Boolean']['Val'], cl['DBL']['Val']] for cl in find_item(seq_arrays, speed + ' analogue array')['Cluster'][na*i:na*(i+1)]])))) for i in range(nsteps)]))
+
+def strip_sequence(seq_dict):
+    """Remove all the extra jargon that LabVIEW puts in XML files
+    from a dictionary containing the experimental sequence."""
+    routine_ind = 0 if seq_dict['LVData']['Cluster']['String'][0]['Name'] == 'Routine name in' else 1
+    return OrderedDict([
+            ('Event list array in',[event_list(name=ev['String']['Val'],
+                    routinespec=ev['Boolean']['Val'], 
+                    indices=[x['Val'] for x in ev['Array']['I32']],
+                    path=ev['Path']['Val']) 
+                for ev in seq_dict['LVData']['Cluster']['Array']['Cluster']]),
+            ('Routine name in', seq_dict['LVData']['Cluster']['String'][routine_ind]['Val']),
+            ('Routine description in', seq_dict['LVData']['Cluster']['String'][int(not routine_ind)]['Val']),
+            ('Experimental sequence cluster in', OrderedDict([
+                *strip_hf(seq_dict['LVData']['Cluster']['Cluster']['Array'], up=1),
+                *strip_hf(seq_dict['LVData']['Cluster']['Cluster']['Array'], up=0)]))])
 
 #### #### Convert json <-> python dict #### ####
 
 class translate:
-    """Write DExTer sequences to json files.
+    """Write DExTer sequences to XML files.
     Facilitate editing of several variables quickly.
     A sequence has a fixed number of events, num_e,
     which give a total number of steps, num_s.
@@ -212,51 +278,52 @@ class translate:
      - routine name
      - routine description
     """
-    def __init__(self, , num_e=1, num_s=1):
-        self.json_dict = OrderedDict([(
-            ('Event list array in'),
-                [event_list()]*num_e),
-            ('Routine name in', (''),
-            ('Routine description in', (''),
-            ('Experimental sequence cluster in', OrderedDict([(
+    def __init__(self, num_e=1, num_s=1):
+        self.nfd = 56 # number of fast digital channels
+        self.nfa = 8  # number of fast analogue
+        self.nsd = 48 # number of slow digital
+        self.nsa = 32 # number of slow analogue
+        self.seq_dic = OrderedDict([
+            ('Event list array in',[event_list()]*num_e),
+            ('Routine name in', ''),
+            ('Routine description in', ''),
+            ('Experimental sequence cluster in', OrderedDict([
                 ('Sequence header top',[header_cluster()]*num_s),
-                ('Fast digital names',channel_names(56)),
-                ('Fast digital channels',[[False]*56]*num_s),
-                ('Fast analogue names',channel_names(8)),
-                ('Fast analogue array',[analogue_cluster(8)]*num_s),
+                ('Fast digital names',channel_names(self.nfd)),
+                ('Fast digital channels',[[False]*self.nfd]*num_s),
+                ('Fast analogue names',channel_names(self.nfa)),
+                ('Fast analogue array',[analogue_cluster(self.nfa)]*num_s),
                 ('Sequence header middle',[header_cluster()]*num_s),
-                ('Slow digital names',channel_names(48)),
-                ('Slow digital channels',[[False]*48]*num_s),
-                ('Slow analogue names',channel_names(32)),
-                ('Slow analogue array',[analogue_cluster(32)]*num_s)])
+                ('Slow digital names',channel_names(self.nsd)),
+                ('Slow digital channels',[[False]*self.nsd]*num_s),
+                ('Slow analogue names',channel_names(self.nsa)),
+                ('Slow analogue array',[analogue_cluster(self.nsa)]*num_s)])
             )])
 
-    def write_to_file(self, fname='sequence_example.json'):
-        """Write the current sequence in the json dictionary
-        format to a file with name fname."""
+    def write_to_file(self, fname='sequence_example.xml'):
+        """Write the current sequence in the dictionary
+        format to an XML file with name fname."""
         with open(fname, 'w+') as f:
-            json.dump(self.json_dict, f, indent=4)
+            f.write(xmltodict.unparse(wrap_sequence(self.seq_dic), 
+                pretty=True).replace(
+                'encoding="utf-8"?', 'standalone="yes" ?'))
         
     def write_to_str(self):
-        """Return the current sequence in the json dictionary
-        format as a string."""
-        return json.dumps(self.json_dict)
+        """Return the current sequence in the dictionary
+        in XML string format specified by LabVIEW"""
+        return xmltodict.unparse(wrap_sequence(self.seq_dic)).replace(
+                'encoding="utf-8"?', 'standalone="yes" ?')
 
-    def load_json(self, fname='sequence_example.json'):
-        """Load a sequence as a dictionary from a json file."""
-        with open(fname, 'r') as f:
-            self.json_dict = json.load(f)
-            
     def load_xml(self, fname='sequence_example.xml'):
         """Load a sequence as a dictionary from an xml file."""
         with open(fname, 'r') as f:
             whole_dict = xmltodict.parse(f.read())
-            self.json_dict = whole_dict # needs restructuring
+            self.seq_dic = strip_sequence(whole_dict) # needs restructuring
 
     def add_event(self, idx=None, event_name=event_list(), 
             header_top=header_cluster(), fd=[False]*56,
             fa=analogue_cluster(8), header_mid=header_cluster(),
-            sd=[False]*8, sa=analogue_cluster(8)):
+            sd=[False]*48, sa=analogue_cluster(32)):
         """Append a new event with the given values at the given
         index.
         idx        -- the index to insert at. Defualt appends at end.
@@ -268,24 +335,24 @@ class translate:
         sd         -- slow digital channels
         sa         -- slow analogue channels."""
         if idx == None:
-            idx = len(self.json_dict['Event list array in'])
-        self.json_dict['Event list array in'].insert(idx, event_name)
-        esc = self.json_dict['Experimental sequence cluster in'] # shorthand
+            idx = len(self.seq_dic['Event list array in'])
+        self.seq_dic['Event list array in'].insert(idx, event_name)
+        esc = self.seq_dic['Experimental sequence cluster in'] # shorthand
         esc['Sequence header top'].insert(idx, header_top)
         esc['Fast digital channels'].insert(idx, fd)
         esc['Fast analogue array'].insert(idx, fa)
         esc['Sequence header middle'].insert(idx, header_mid)
-        esc['Slow digital channels'].insert(sd)
-        esc['Slow analogue array'].insert(sa)
+        esc['Slow digital channels'].insert(idx, sd)
+        esc['Slow analogue array'].insert(idx, sa)
 
     # extra functions for checking the sequence is correct format?
 
-    @staticmethod
-    def get_mr_xml_str(mr_array, anlogue_options, change_type, 
-                        analogue_channel, time_step):
-        """Convert the multirun array of values, along with the
-        selected channels, into an XML string to send to DExTer"""
-        dicttoxml
+    # @staticmethod
+    # def get_mr_xml_str(mr_array, anlogue_options, change_type, 
+    #                     analogue_channel, time_step):
+    #     """Convert the multirun array of values, along with the
+    #     selected channels, into an XML string to send to DExTer"""
+    #     dicttoxml
 
 
 #### #### Edit sequences #### ####
@@ -577,4 +644,6 @@ def run():
         sys.exit(app.exec_()) # when the window is closed, the python code also stops
    
 if __name__ == "__main__":
-    run()
+    # run()
+    t = translate()
+    t.load_xml(r'Y:\Tweezer\People\Stefan\0_17 September 2019_09 30 18.xml')
